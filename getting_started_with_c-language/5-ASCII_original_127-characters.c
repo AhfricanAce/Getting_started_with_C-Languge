e271e0ac1a73d1f4f98e6c3e21bf90f059db12b1
MAC <aoamacsplace@victor-Knight.local>
2023-07-28 12:00:50 +0100

    C-language

---
+++ getting_started_with_c-language/5-ASCII_original_127-characters.c
@@ -0,0 +1,5 @@
+# Follow link to see how the original 127-character ASCII set is considered by
+each of the classifying type functions 
+(PS: a (.) indicates that the function returns non-zero for that character)
+
+http://www.cplusplus.com/reference/cctype/
